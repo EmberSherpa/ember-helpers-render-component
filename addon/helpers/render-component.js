fd66156f92d66548eae730d581222e902f29aa79
Taras Mankovski <taras@quandl.com>
2014-11-23 20:42:03 -0500

    Made it possible to pass a bound component name

--- addon/helpers/render-component.js
+++ addon/helpers/render-component.js
@@ -1,6 +1,14 @@
 import Ember from 'ember';
 
 export default function renderComponent(component, options) {
-  var helper = Ember.Handlebars.resolveHelper(options.data.view.container, component);
+
+  var helperName;
+  if (options.types[0] === "ID") {
+    helperName = Ember.Handlebars.get(this, component);
+  } else {
+    helperName = component;
+  }
+
+  var helper = Ember.Handlebars.resolveHelper(options.data.view.container, helperName);
   return helper.call(this, options);
 }
